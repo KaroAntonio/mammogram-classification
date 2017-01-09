813356bdc8eb64b31e0718c48e27e9a1bdfe3ad1
James Yuan <jamesy94@gmail.com>
2017-01-09 15:43:21 -0500

    Convert tabs to spaces. Also update requirements.txt with new requirements.

--- preprocess/generate_image_labels.py
+++ preprocess/generate_image_labels.py
@@ -16,57 +16,58 @@ Author: Thomas Schaffter (thomas.schaff...@gmail.com)
 Last update: 2016-11-02
 """
 
-import numpy as np
-import pandas as pd
-import sys
 import os
+import sys
+
+import pandas as pd
+
 
 def generateImageLabels(images, metadata):
-	"""
-	Generates the cancer label for every image listed in the images
-	crosswalk table using cancerL and cancerR from the exams metadata table.
+    """
+    Generates the cancer label for every image listed in the images
+    crosswalk table using cancerL and cancerR from the exams metadata table.
 
-	Args:
-	    images: images crosswalk table
-	    metadata: exams metadata table
-	Returns:
-	    a table with the columns image 'filename' and 'cancer'.
-	"""
-	metadata['examId'] = metadata.subjectId+("_")+metadata.examIndex.astype(str)
-        images['examId'] = images.subjectId+("_")+images.examIndex.astype(str)
-        examIdsCancerL = metadata.examId[metadata.cancerL == 1]
-        examIdsCancerR = metadata.examId[metadata.cancerR == 1]
-        images['cancer'] = 0
-        images.loc[(images.examId.isin(examIdsCancerL)) & (images.laterality == "L"), 'cancer'] = 1
-        images.loc[(images.examId.isin(examIdsCancerR)) & (images.laterality == "R"), 'cancer'] = 1
-	
-	return images[['filename', 'cancer']].copy()
+    Args:
+        images: images crosswalk table
+        metadata: exams metadata table
+    Returns:
+        a table with the columns image 'filename' and 'cancer'.
+    """
+    metadata['examId'] = metadata.subjectId + ("_") + metadata.examIndex.astype(str)
+    images['examId'] = images.subjectId + ("_") + images.examIndex.astype(str)
+    examIdsCancerL = metadata.examId[metadata.cancerL == 1]
+    examIdsCancerR = metadata.examId[metadata.cancerR == 1]
+    images['cancer'] = 0
+    images.loc[(images.examId.isin(examIdsCancerL)) & (images.laterality == "L"), 'cancer'] = 1
+    images.loc[(images.examId.isin(examIdsCancerR)) & (images.laterality == "R"), 'cancer'] = 1
+
+    return images[['filename', 'cancer']].copy()
 
 
 if __name__ == '__main__':
-        examsMetadataFilename = sys.argv[1]
-	imagesCrosswalkFilename = sys.argv[2]
-	labelsFilename = sys.argv[3]
+    examsMetadataFilename = sys.argv[1]
+    imagesCrosswalkFilename = sys.argv[2]
+    labelsFilename = sys.argv[3]
 
-	# Read the label from the exams metadata
-	fields = ['subjectId', 'examIndex', 'cancerL', 'cancerR']
-	metadata = pd.read_csv(examsMetadataFilename, sep="\t", na_values='.',usecols=fields)
+    # Read the label from the exams metadata
+    fields = ['subjectId', 'examIndex', 'cancerL', 'cancerR']
+    metadata = pd.read_csv(examsMetadataFilename, sep="\t", na_values='.', usecols=fields)
 
-	# Read the data from the images crosswalk file
-	fields = ['subjectId', 'examIndex', 'filename', 'laterality']
-	images = pd.read_csv(imagesCrosswalkFilename, sep="\t", na_values='.', usecols=fields)
+    # Read the data from the images crosswalk file
+    fields = ['subjectId', 'examIndex', 'filename', 'laterality']
+    images = pd.read_csv(imagesCrosswalkFilename, sep="\t", na_values='.', usecols=fields)
 
-	# Convert subjectId to string
-	metadata.subjectId = metadata.subjectId.astype(str)
-	images.subjectId = images.subjectId.astype(str)
+    # Convert subjectId to string
+    metadata.subjectId = metadata.subjectId.astype(str)
+    images.subjectId = images.subjectId.astype(str)
 
-	labels = generateImageLabels(images, metadata)
-	#labels['key'] = labels.filename.str.replace('.dcm', '')
-	#labels.loc[:,'filename'] = labels.filename.str.replace('.dcm', '.png')
-	#labels.loc[:,'filename'] = labels.filename.str.replace('.dcm', '')
+    labels = generateImageLabels(images, metadata)
+    # labels['key'] = labels.filename.str.replace('.dcm', '')
+    # labels.loc[:,'filename'] = labels.filename.str.replace('.dcm', '.png')
+    # labels.loc[:,'filename'] = labels.filename.str.replace('.dcm', '')
 
-	# Export the labels to file
-	directory = os.path.dirname(labelsFilename)
-	if not os.path.isdir(directory):
-		os.makedirs(directory)
-	labels.to_csv(labelsFilename, sep=' ', index=False, header=False)
+    # Export the labels to file
+    directory = os.path.dirname(labelsFilename)
+    if not os.path.isdir(directory):
+        os.makedirs(directory)
+    labels.to_csv(labelsFilename, sep=' ', index=False, header=False)
